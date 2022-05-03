11c69e1253cf971662dc1cfe13f915f4f48de576
sm02 <starzary@naver.com>
2022-05-03 16:48:24 +0900

    RunningApp : Handling the Map's Lifecycle 까지

--- app/src/main/java/com/androiddevs/runningapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/androiddevs/runningapp/ui/fragments/SetupFragment.kt
@@ -3,26 +3,18 @@ package com.androiddevs.runningapp.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import com.androiddevs.runningapp.R
-import com.androiddevs.runningapp.ui.viewmodels.MainViewModel
-import com.androiddevs.runningapp.ui.viewmodels.StatisticsViewModel
 import androidx.navigation.fragment.findNavController
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_run.*
+import com.androiddevs.runningapp.R
 import kotlinx.android.synthetic.main.fragment_setup.*
-import kotlinx.android.synthetic.main.fragment_setup.tvContinue
 
-@AndroidEntryPoint
 class SetupFragment : Fragment(R.layout.fragment_setup) {
-
-    private val viewModel: StatisticsViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        fab.setOnClickListener {
-            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
         }
     }
+
 }
