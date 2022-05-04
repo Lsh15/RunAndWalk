fdf8cf9d28cec01317b443db7f77cae143ca2d2f
sm02 <starzary@naver.com>
2022-05-04 17:17:20 +0900

    RunningApp : Tracking Service Basic Setup 까지

--- app/src/main/java/com/androiddevs/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/androiddevs/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,10 +1,13 @@
 package com.androiddevs.runningapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.androiddevs.runningapp.R
+import com.androiddevs.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.androiddevs.runningapp.services.TrackingService
 import com.androiddevs.runningapp.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,10 +24,20 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         super.onViewCreated(view, savedInstanceState)
         mapView.onCreate(savedInstanceState)
 
+        btnToggleRun.setOnClickListener{
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
         mapView.getMapAsync {
             map = it
         }
     }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
+
 
     override fun onResume() {
         super.onResume()
