af6f811f1e76424b93640a76d8ae7ca3963cf96e
SeunghHun <starzary@naver.com>
2022-05-12 23:18:52 +0900

    RunningApp : Saving a Run in the Database 까지

--- app/src/main/java/com/androiddevs/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/androiddevs/runningapp/ui/fragments/TrackingFragment.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.androiddevs.runningapp.R
 import com.androiddevs.runningapp.R.menu
+import com.androiddevs.runningapp.db.Run
 import com.androiddevs.runningapp.other.Constants.ACTION_PAUSE_SERVICE
 import com.androiddevs.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.androiddevs.runningapp.other.Constants.ACTION_STOP_SERVICE
@@ -23,10 +24,15 @@ import com.androiddevs.runningapp.services.TrackingService
 import com.androiddevs.runningapp.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
+import java.util.*
+import kotlin.math.round
+
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
@@ -40,6 +46,8 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     private var curTimeInMillis = 0L
 
     private var menu: Menu? = null
+
+    private var weight = 80f
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -55,6 +63,10 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         mapView.onCreate(savedInstanceState)
         btnToggleRun.setOnClickListener {
             toggleRun()
+        }
+        btnFinishRun.setOnClickListener{
+            zoomToSeeWholeTrack()
+            endRunAndSaveToDb()
         }
         mapView.getMapAsync {
             map = it
@@ -153,6 +165,45 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
                     MAP_ZOOM
                 )
             )
+        }
+    }
+
+    private fun zoomToSeeWholeTrack() {
+        val bounds = LatLngBounds.Builder()
+        for(polyline in pathPoints) {
+            for(pos in polyline) {
+                bounds.include(pos)
+            }
+        }
+
+        map?.moveCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                bounds.build(),
+                mapView.width,
+                mapView.height,
+                (mapView.height * 0.05f).toInt()
+            )
+        )
+    }
+
+
+    private fun endRunAndSaveToDb() {
+        map?.snapshot { bmp ->
+            var distanceInMeters = 0
+            for(polyline in pathPoints) {
+                distanceInMeters += TrackingUtility.calculatePolylineLength(polyline).toInt()
+            }
+            val avgSpeed = round((distanceInMeters / 1000f) / (curTimeInMillis / 1000f / 60 / 60) * 10) / 10f
+            val dateTimestamp = Calendar.getInstance().timeInMillis
+            val caloriesBurned = ((distanceInMeters / 1000f) * weight).toInt()
+            val run = Run(bmp, dateTimestamp, avgSpeed, distanceInMeters, curTimeInMillis, caloriesBurned)
+            viewModel.insertRun(run)
+            Snackbar.make(
+                requireActivity().findViewById(R.id.rootView),
+                "Run saved successfully",
+                Snackbar.LENGTH_LONG
+            ).show()
+            stopRun()
         }
     }
 
--- app/src/main/java/com/androiddevs/runningapp/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/androiddevs/runningapp/ui/viewmodels/MainViewModel.kt
@@ -2,10 +2,18 @@ package com.androiddevs.runningapp.ui.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.runningapp.db.Run
 import com.androiddevs.runningapp.repositories.MainRepository
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+
+
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
