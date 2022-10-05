f6239b0c11f180a69bcb0435acabc2a042707353
SeunghHun <starzary@naver.com>
2022-10-05 21:58:07 +0900

    Edit Constants.kt

--- app/src/main/java/com/androiddevs/runningapp/other/Constants.kt
+++ app/src/main/java/com/androiddevs/runningapp/other/Constants.kt
@@ -19,6 +19,7 @@ object Constants {
     const val KEY_FIRST_TIME_TOGGLE = "KEY_FIRST_TIME_TOGGLE"
     const val KEY_NAME = "KEY_NAME"
     const val KEY_WEIGHT = "KEY_WEIGHT"
+    const val KEY_TYPE = "KEY_TYPE"
 
     const val LOCATION_UPDATE_INTERVAL = 5000L
     const val FASTEST_LOCATION_INTERVAL = 2000L
