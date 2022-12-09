b440cd1a3e1a14b56d2c9466c720c5de562a4136
SeunghHun <starzary@naver.com>
2022-12-09 17:20:08 +0900

    Edit SettingsFragment.kt

--- app/src/main/java/com/androiddevs/runningapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/androiddevs/runningapp/ui/fragments/SettingsFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.androiddevs.runningapp.R
+import com.androiddevs.runningapp.other.Constants
 import com.androiddevs.runningapp.other.Constants.KEY_NAME
 import com.androiddevs.runningapp.other.Constants.KEY_TYPE
 import com.androiddevs.runningapp.other.Constants.KEY_WEIGHT
@@ -23,6 +24,9 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
     @Inject
     lateinit var sharedPreferences: SharedPreferences
+
+    // 현재 line 색상
+    private var colorState : Int = 1
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -48,6 +52,35 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
                 Snackbar.make(view, "Please fill out all the fields", Snackbar.LENGTH_LONG).show()
             }
         }
+
+        // 색상 원 선택 시
+        redCircle.setOnClickListener {
+            colorState = 1
+            selectColorCircle()
+        }
+        blueCircle.setOnClickListener {
+            colorState = 2
+            selectColorCircle()
+        }
+        greenCircle.setOnClickListener {
+            colorState = 3
+            selectColorCircle()
+        }
+        blackCircle.setOnClickListener {
+            colorState = 4
+            selectColorCircle()
+        }
+        // 선 색 변경 버튼 클릭 시
+        btnLineChanges.setOnClickListener {
+            val success = changingColor()
+            if(success){
+                Snackbar.make(view,"Line color changed.",Snackbar.LENGTH_SHORT).show()
+            }
+            else{
+                Snackbar.make(view,"Please choose again.",Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
     private fun loadFieldsFromSharedPref() {
@@ -74,8 +107,48 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
             .putString(KEY_NAME, nameText)
             .putFloat(KEY_WEIGHT, weightText.toFloat())
             .apply()
-        val toolbarText = "Let's go $nameText"
+//        val toolbarText = "Let's go $nameText"
+        val toolbarText = getString(R.string.HELLO) + " " + nameText + "!"
         requireActivity().tvToolbarTitle.text = toolbarText
         return true
+    }
+
+    // 색상 원 선택 체크
+    private fun selectColorCircle(){
+            when (colorState) {
+                1 -> {
+                    redCheck.visibility = View.VISIBLE
+                    blueCheck.visibility = View.GONE
+                    greenCheck.visibility = View.GONE
+                    blackCheck.visibility = View.GONE
+                }
+                2 -> {
+                    redCheck.visibility = View.GONE
+                    blueCheck.visibility = View.VISIBLE
+                    greenCheck.visibility = View.GONE
+                    blackCheck.visibility = View.GONE
+                }
+                3 -> {
+                    redCheck.visibility = View.GONE
+                    blueCheck.visibility = View.GONE
+                    greenCheck.visibility = View.VISIBLE
+                    blackCheck.visibility = View.GONE
+                }
+                4 -> {
+                    redCheck.visibility = View.GONE
+                    blueCheck.visibility = View.GONE
+                    greenCheck.visibility = View.GONE
+                    blackCheck.visibility = View.VISIBLE
+            }
+         }
+    }
+
+    // 선 색상 변경
+    private fun changingColor() : Boolean {
+            // 선 색상 정보 저장
+        sharedPreferences.edit()
+            .putInt(Constants.KEY_COLOR, colorState)
+            .apply()
+            return true
     }
 }
